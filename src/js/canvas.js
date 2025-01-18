77ef15f5701538391b5c81c6c704934aa44bbfae
Robin Clarke <25224498+robinclarke95@users.noreply.github.com>
2025-01-18 07:55:43 -0800

    Import named functions from utils.js
    utils.js exports multiple named exports
    import utils from './utils.js';  only imports a single function

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,4 @@
-import utils from './utils'
+import { randomIntFromRange, randomColor, distance } from './utils'
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
