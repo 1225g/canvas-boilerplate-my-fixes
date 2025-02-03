6ddda31147933044453c0cdc82814fc4be420588
Robin Clarke <robinc@blackdata.ca>
2025-02-03 20:23:00 +0000

    Changes

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -104,9 +104,12 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
@@ -116,7 +119,6 @@ var mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 };
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
 
 // Event Listeners
 addEventListener('mousemove', function (event) {
@@ -128,22 +130,43 @@ addEventListener('resize', function () {
   canvas.height = innerHeight;
   init();
 });
-
-// Objects
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+var Boundary = /*#__PURE__*/function () {
+  function Boundary(_ref) {
+    var position = _ref.position,
+      image = _ref.image;
+    _classCallCheck(this, Boundary);
+    this.position = position;
+    this.width = 40;
+    this.height = 40;
+    this.image = image;
   }
-  _createClass(Object, [{
+  _createClass(Boundary, [{
+    key: "draw",
+    value: function draw() {
+      //c.fillStyle = 'blue'
+      //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }]);
+  return Boundary;
+}();
+_defineProperty(Boundary, "width", 40);
+_defineProperty(Boundary, "height", 40);
+var Player = /*#__PURE__*/function () {
+  function Player(_ref2) {
+    var position = _ref2.position,
+      velocity = _ref2.velocity;
+    _classCallCheck(this, Player);
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 15;
+  }
+  _createClass(Player, [{
     key: "draw",
     value: function draw() {
       c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
+      c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+      c.fillStyle = 'yellow';
       c.fill();
       c.closePath();
     }
@@ -151,29 +174,178 @@ var _Object = /*#__PURE__*/function () {
     key: "update",
     value: function update() {
       this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
     }
   }]);
-  return Object;
-}(); // Implementation
-var objects;
-function init() {
-  objects = [];
-  for (var i = 0; i < 400; i++) {
-    objects.push();
+  return Player;
+}();
+var boundaries = [];
+var player = new Player({
+  position: {
+    x: Boundary.width + Boundary.width / 2,
+    y: Boundary.height + Boundary.height / 2
+  },
+  velocity: {
+    x: 0,
+    y: 0
   }
+});
+var keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+};
+var lastKey = '';
+var map = [['-', '-', '-', '-', '-', '-', '-'], ['-', ' ', ' ', ' ', ' ', ' ', '-'], ['-', ' ', '-', ' ', '-', ' ', '-'], ['-', ' ', ' ', ' ', ' ', ' ', '-'], ['-', ' ', '-', ' ', '-', ' ', '-'], ['-', ' ', ' ', ' ', ' ', ' ', '-'], ['-', '-', '-', '-', '-', '-', '-']];
+var image = new Image();
+image.src = '../img/pipeHorizontal.png';
+map.forEach(function (row, i) {
+  row.forEach(function (symbol, j) {
+    switch (symbol) {
+      case '-':
+        boundaries.push(new Boundary({
+          position: {
+            x: Boundary.width * j,
+            y: Boundary.height * i
+          },
+          image: image
+        }));
+        break;
+    }
+  });
+});
+function circleCollidesWithREctangle(_ref3) {
+  var circle = _ref3.circle,
+    rectangle = _ref3.rectangle;
+  return circle.position.y - circle.radius + circle.velocity.y <= rectangle.position.y + rectangle.height && circle.position.x + circle.radius + circle.velocity.x >= rectangle.position.x && circle.position.y + circle.radius + circle.velocity.y >= rectangle.position.y && circle.position.x - circle.radius + circle.velocity.x <= rectangle.position.x + rectangle.width;
 }
-
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y);
-  objects.forEach(function (object) {
-    object.update();
+  if (keys.w.pressed && lastKey === 'w') {
+    for (var i = 0; i < boundaries.length; i++) {
+      var boundary = boundaries[i];
+      if (circleCollidesWithREctangle({
+        circle: _objectSpread({}, player, {
+          velocity: {
+            x: 0,
+            y: -5
+          }
+        }),
+        rectangle: boundary
+      })) {
+        player.velocity.y = 0;
+        break;
+      } else player.velocity.y = -5;
+    }
+  } else if (keys.a.pressed && lastKey === 'a') {
+    for (var _i = 0; _i < boundaries.length; _i++) {
+      var _boundary = boundaries[_i];
+      if (circleCollidesWithREctangle({
+        circle: _objectSpread({}, player, {
+          velocity: {
+            x: -5,
+            y: 0
+          }
+        }),
+        rectangle: _boundary
+      })) {
+        player.velocity.x = 0;
+        break;
+      } else player.velocity.x = -5;
+    }
+  } else if (keys.s.pressed && lastKey === 's') {
+    for (var _i2 = 0; _i2 < boundaries.length; _i2++) {
+      var _boundary2 = boundaries[_i2];
+      if (circleCollidesWithREctangle({
+        circle: _objectSpread({}, player, {
+          velocity: {
+            x: 0,
+            y: 5
+          }
+        }),
+        rectangle: _boundary2
+      })) {
+        player.velocity.y = 0;
+        break;
+      } else player.velocity.y = 5;
+    }
+  } else if (keys.d.pressed && lastKey === 'd') {
+    for (var _i3 = 0; _i3 < boundaries.length; _i3++) {
+      var _boundary3 = boundaries[_i3];
+      if (circleCollidesWithREctangle({
+        circle: _objectSpread({}, player, {
+          velocity: {
+            x: 5,
+            y: 0
+          }
+        }),
+        rectangle: _boundary3
+      })) {
+        player.velocity.x = 0;
+        break;
+      } else player.velocity.x = 5;
+    }
+  }
+  boundaries.forEach(function (boundary) {
+    boundary.draw();
+    if (circleCollidesWithREctangle({
+      circle: player,
+      rectangle: boundary
+    })) {
+      player.velocity.x = 0;
+      player.velocity.y = 0;
+    }
   });
+  player.update();
 }
-init();
 animate();
+addEventListener('keydown', function (_ref4) {
+  var key = _ref4.key;
+  switch (key) {
+    case 'w':
+      keys.w.pressed = true;
+      break;
+    case 'a':
+      keys.a.pressed = true;
+      break;
+    case 's':
+      keys.s.pressed = true;
+      break;
+    case 'd':
+      keys.d.pressed = true;
+      break;
+  }
+  lastKey = key;
+});
+addEventListener('keyup', function (_ref5) {
+  var key = _ref5.key;
+  switch (key) {
+    case 'w':
+      keys.w.pressed = false;
+      break;
+    case 'a':
+      keys.a.pressed = false;
+      break;
+    case 's':
+      keys.s.pressed = false;
+      break;
+    case 'd':
+      keys.d.pressed = false;
+      break;
+  }
+});
 })();
 
 /******/ })()
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -11,8 +11,6 @@ const mouse = {
   y: innerHeight / 2
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
-
 // Event Listeners
 addEventListener('mousemove', (event) => {
   mouse.x = event.clientX
@@ -26,48 +24,220 @@ addEventListener('resize', () => {
   init()
 })
 
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+class Boundary {
+  static width = 40
+  static height = 40
+  constructor ({position, image}) {
+    this.position = position
+    this.width = 40
+    this.height = 40
+    this.image = image
+  }
+
+  draw() {
+    //c.fillStyle = 'blue'
+    //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
+}
+
+class Player {
+  constructor({position, velocity}) {
+    this.position = position
+    this.velocity = velocity
+    this.radius = 15
   }
 
   draw() {
     c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    c.fillStyle = 'yellow'
     c.fill()
     c.closePath()
   }
 
   update() {
     this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
   }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
 
-  for (let i = 0; i < 400; i++) {
-    objects.push()
+const boundaries = []
+const player = new Player({
+  position: {
+    x: Boundary.width + Boundary.width / 2,
+    y: Boundary.height + Boundary.height / 2,
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
+})
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
   }
 }
 
+let lastKey = ''
+
+const map = [
+  ['-','-','-','-','-','-','-'],
+  ['-',' ',' ',' ',' ',' ','-'],
+  ['-',' ','-',' ','-',' ','-'],
+  ['-',' ',' ',' ',' ',' ','-'],
+  ['-',' ','-',' ','-',' ','-'],
+  ['-',' ',' ',' ',' ',' ','-'],
+  ['-','-','-','-','-','-','-'],
+]
+
+const image = new Image()
+image.src = '../img/pipeHorizontal.png'
+
+map.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    switch(symbol){
+      case '-':
+        boundaries.push(new Boundary({
+          position: {
+            x: Boundary.width * j,
+            y: Boundary.height * i
+          },
+          image: image
+        }))
+        break;
+
+    }
+  })
+})
+
+
+function circleCollidesWithREctangle({circle,rectangle}){
+  return (circle.position.y - circle.radius + circle.velocity.y <= rectangle.position.y + rectangle.height
+    && circle.position.x + circle.radius + circle.velocity.x >= rectangle.position.x
+    && circle.position.y + circle.radius + circle.velocity.y >= rectangle.position.y
+    && circle.position.x - circle.radius + circle.velocity.x <= rectangle.position.x + rectangle.width
+  )
+}
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
+  
+  if (keys.w.pressed && lastKey === 'w') {
+    for (let i=0; i< boundaries.length; i++) {
+      const boundary = boundaries[i]
+      if (circleCollidesWithREctangle({circle: {...player, velocity: {
+        x: 0,
+        y: -5
+      }}, rectangle: boundary})) {
+        player.velocity.y = 0
+        break
+      }
+      else
+        player.velocity.y = -5
+    }
+  } else if (keys.a.pressed && lastKey === 'a') {
+    for (let i=0; i< boundaries.length; i++) {
+      const boundary = boundaries[i]
+      if (circleCollidesWithREctangle({circle: {...player, velocity: {
+        x: -5,
+        y: 0
+      }}, rectangle: boundary})) {
+        player.velocity.x = 0
+        break
+      }
+      else
+        player.velocity.x = -5
+    }
+  } else if (keys.s.pressed && lastKey === 's') {
+    for (let i=0; i< boundaries.length; i++) {
+      const boundary = boundaries[i]
+      if (circleCollidesWithREctangle({circle: {...player, velocity: {
+        x: 0,
+        y: 5
+      }}, rectangle: boundary})) {
+        player.velocity.y = 0
+        break
+      }
+      else
+        player.velocity.y = 5
+    }
+  } else if (keys.d.pressed && lastKey === 'd') {
+    for (let i=0; i< boundaries.length; i++) {
+      const boundary = boundaries[i]
+      if (circleCollidesWithREctangle({circle: {...player, velocity: {
+        x: 5,
+        y: 0
+      }}, rectangle: boundary})) {
+        player.velocity.x = 0
+        break
+      }
+      else
+        player.velocity.x = 5
+    }
+  }
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  objects.forEach(object => {
-   object.update()
+  boundaries.forEach((boundary) => {
+    boundary.draw()
+
+    if (circleCollidesWithREctangle({circle: player, rectangle: boundary})) {
+      player.velocity.x = 0
+      player.velocity.y = 0
+    }
   })
+
+  player.update()
+ 
+
+
+
 }
 
-init()
 animate()
+
+addEventListener('keydown', ({key}) => {
+  switch(key) {
+    case 'w':
+      keys.w.pressed = true
+      break
+    case 'a':
+      keys.a.pressed = true
+      break
+    case 's':
+      keys.s.pressed = true
+      break
+    case 'd':
+      keys.d.pressed = true
+      break
+  }
+  lastKey = key
+})
+
+addEventListener('keyup', ({key}) => {
+  switch(key) {
+    case 'w':
+      keys.w.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+    case 's':
+      keys.s.pressed = false
+      break
+    case 'd':
+      keys.d.pressed = false
+      break
+    }
+})
