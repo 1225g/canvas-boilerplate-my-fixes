3cf01ed3b8b98a7f481c4fabbf39b9cdcd6a41d5
Robin Clarke <robinc@blackdata.ca>
2025-02-06 22:00:47 +0000

    Added Boundary

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -1,182 +1,25 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/js/utils.js":
-/*!*************************!*\
-  !*** ./src/js/utils.js ***!
-  \*************************/
-/***/ ((module) => {
+/***/ "./src/js/canvas.js":
+/*!**************************!*\
+  !*** ./src/js/canvas.js ***!
+  \**************************/
+/***/ (() => {
 
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-function distance(x1, y1, x2, y2) {
-  var xDist = x2 - x1;
-  var yDist = y2 - y1;
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-}
-module.exports = {
-  randomIntFromRange: randomIntFromRange,
-  randomColor: randomColor,
-  distance: distance
-};
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /home/debian/web/canvas-boilerplate-my-fixes/src/js/canvas.js: Missing semicolon. (26:25)\n\n\u001b[0m \u001b[90m 24 |\u001b[39m \u001b[36mclass\u001b[39m \u001b[33mBoundary\u001b[39m {\n \u001b[90m 25 |\u001b[39m   \u001b[36mstatic\u001b[39m width \u001b[33m=\u001b[39m \n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 26 |\u001b[39m   constructor({position}) {\n \u001b[90m    |\u001b[39m                          \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 27 |\u001b[39m     \u001b[36mthis\u001b[39m\u001b[33m.\u001b[39mposition \u001b[33m=\u001b[39m position\n \u001b[90m 28 |\u001b[39m     \u001b[36mthis\u001b[39m\u001b[33m.\u001b[39mwidth \u001b[33m=\u001b[39m \u001b[35m48\u001b[39m\n \u001b[90m 29 |\u001b[39m     \u001b[36mthis\u001b[39m\u001b[33m.\u001b[39mheight \u001b[33m=\u001b[39m \u001b[35m48\u001b[39m\u001b[0m\n    at constructor (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:360:19)\n    at Parser.raise (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:3327:19)\n    at Parser.semicolon (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:3653:10)\n    at Parser.parseClassProperty (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:13375:10)\n    at Parser.pushClassProperty (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:13338:30)\n    at Parser.parseClassMemberWithIsStatic (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:13249:14)\n    at Parser.parseClassMember (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:13193:10)\n    at /home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:13147:14\n    at Parser.withSmartMixTopicForbiddingContext (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:12067:14)\n    at Parser.parseClassBody (/home/debian/web/canvas-boilerplate-my-fixes/node_modules/@babel/parser/lib/index.js:13129:10)");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
 /******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/canvas.js"]();
 /******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-// This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
-(() => {
-"use strict";
-/*!**************************!*\
-  !*** ./src/js/canvas.js ***!
-  \**************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
-/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-var mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
-};
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
-
-// Event Listeners
-addEventListener('mousemove', function (event) {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
-addEventListener('resize', function () {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  init();
-});
-
-// Objects
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-  }
-  _createClass(Object, [{
-    key: "draw",
-    value: function draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-      c.closePath();
-    }
-  }, {
-    key: "update",
-    value: function update() {
-      this.draw();
-    }
-  }]);
-  return Object;
-}(); // Implementation
-var objects;
-function init() {
-  objects = [];
-  for (var i = 0; i < 400; i++) {
-    objects.push();
-  }
-}
-
-// Animation Loop
-function animate() {
-  requestAnimationFrame(animate);
-  c.fillStyle = 'white';
-  c.fillRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y);
-  objects.forEach(function (object) {
-    object.update();
-  });
-}
-init();
-animate();
-})();
-
 /******/ })()
 ;
 //# sourceMappingURL=canvas.bundle.js.map
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,14 @@
-import { randomIntFromRange, randomColor, distance } from './utils'
+import { randomIntFromRange, randomColor, distance } from './utils.js'
+
+import {collisions} from '../data/collisions.js'
+
+import pelletTownPng from '../img/Pellet Town.png'
+import playerDownPng from '../img/playerDown.png'
+import playerLeftPng from '../img/playerLeft.png'
+import playerRightPng from '../img/playerRight.png'
+import playerUpPng from '../img/playerUp.png'
+
+
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
@@ -6,28 +16,59 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+const collisionsMap = []
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, 70 + i))
+}
+
+class Boundary {
+  static width = 48
+  static height = 48
+  constructor({position}) {
+    this.position = position
+    this.width = 48
+    this.height = 48
+  }
+
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+
+const boundaries = []
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(new Boundary({position: {
+        x: j * Boundary.width,
+        y: i * Boundary.height
+      }
+      }))
+  })
+})
+
+const image = new Image()
+image.src = pelletTownPng
+
+const playerImage = new Image()
+playerImage.src = playerDownPng
+
 const mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
-
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
 
 addEventListener('resize', () => {
   canvas.width = innerWidth
   canvas.height = innerHeight
 
-  init()
 })
 
 // Objects
-class Object {
+class Player {
   constructor(x, y, radius, color) {
     this.x = x
     this.y = y
@@ -48,27 +89,100 @@ class Object {
   }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
-
-  for (let i = 0; i < 400; i++) {
-    objects.push()
+class Sprite {
+  constructor({position, velocity, image}) {
+    this.position = position
+    this.image = image
+  }
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
+const background = new Sprite({ position: {
+  x: -480,
+  y: -500
+  },
+  image: image
+})
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+}
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate)
-  c.fillStyle = 'white'
-  c.fillRect(0, 0, canvas.width, canvas.height)
+  //c.fillStyle = 'white'
+  //c.fillRect(0, 0, canvas.width, canvas.height)
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  objects.forEach(object => {
-   object.update()
+  background.draw()
+
+  boundaries.forEach(boundary => {
+    boundary.draw()
   })
+
+  c.drawImage(playerImage, 
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 2, 
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height)
+
+  if (keys.w.pressed) background.position.y += 3
+  if (keys.a.pressed) background.position.x += 3
+  if (keys.s.pressed) background.position.y -= 3
+  if (keys.d.pressed) background.position.x -= 3
 }
 
-init()
 animate()
+
+let lastKey = ''
+
+addEventListener('keydown', (e) => {
+  switch(e.key) {
+    case 'w':
+      keys.w.pressed = true
+      break
+    case 'a':
+      keys.a.pressed = true
+      break
+    case 's':
+      keys.s.pressed = true
+      break
+    case 'd':
+      keys.d.pressed = true
+      break
+  }
+  lastKey = e.key
+})
+
+addEventListener('keyup', (e) => {
+  switch(e.key) {
+    case 'w':
+      keys.w.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+    case 's':
+      keys.s.pressed = false
+      break
+    case 'd':
+      keys.d.pressed = false
+      break
+  }
+})
