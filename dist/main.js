be6102042633ecd0121857ad98bb8ebbc8cc985d
Robin Clarke <robinc@blackdata.ca>
2025-02-11 17:03:09 +0000

    shop

--- dist/main.js
+++ dist/main.js
@@ -15,6 +15,15 @@ var background = new Sprite({
   },
   imageSrc: '../img/background.png'
 });
+var shop = new Sprite({
+  position: {
+    x: 600,
+    y: 128
+  },
+  imageSrc: '../img/shop.png',
+  scale: 2.75,
+  framesMax: 6
+});
 var player = new Fighter({
   position: {
     x: 0,
@@ -67,6 +76,7 @@ function animate() {
   c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
+  shop.update();
   player.update();
   enemy.update();
   player.velocity.x = 0;
